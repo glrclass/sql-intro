e8fadb97ea76917a6b11f7357aca7a93e9931396
G R <gabi.romagnoli@kellogg.northwestern.edu>
2025-01-16 02:58:49 +0000

    running first pass on lab 4

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,22 @@
 -- How many lifetime hits does Barry Bonds have?
 
+SELECT first_name, last_name
+FROM players
+WHERE first_name = "Barry"
+AND last_name = "Bonds";
+
+SELECT *
+FROM players
+INNER JOIN stats ON players.id = stats.player_ID
+WHERE first_name = "Barry"
+AND last_name = "Bonds";
+
+SELECT SUM(stats.hits)
+FROM players
+INNER JOIN stats ON players.id = stats.player_ID
+WHERE first_name = "Barry"
+AND last_name = "Bonds";
+
 -- Expected result:
 -- 2935
 
